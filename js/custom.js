cc0dd1f1734369a121e48cfcdddb3ec9e03ce0b7
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2022-10-20 03:12:32 +0600

    banner part is completed

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+//banner slider
+$('.banner-slick-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<span class="prev-arr"><i class="fa-solid fa-circle-arrow-left"></i></span>',
+    nextArrow: '<span class="next-arr"><i class="fa-solid fa-circle-arrow-right"></i></span>'
+  });
