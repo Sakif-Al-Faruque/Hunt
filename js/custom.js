0456e7d0dd61a6ec8a96adfba84b19032cc03a69
Sakif-Al-Faqurue <sakif.saf915@gmail.com>
2023-01-08 23:05:16 +0600

    service part is done

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,14 @@ $('.banner-slick-slider').slick({
     prevArrow: '<span class="prev-arr"><i class="fa-solid fa-circle-arrow-left"></i></span>',
     nextArrow: '<span class="next-arr"><i class="fa-solid fa-circle-arrow-right"></i></span>'
   });
+
+  //service slider
+  $('.service-content').slick({
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    prevArrow: '<span class="prev-arr"><i class="fa-solid fa-angle-up"></i></i></span>',
+    nextArrow: '<span class="next-arr"><i class="fa-solid fa-angle-down"></i></span>',
+    vertical: true
+  });
