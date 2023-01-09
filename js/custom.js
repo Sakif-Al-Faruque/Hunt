1a18beb1bcb1f7cb29563d2023c853a14e8e5ce4
Sakif-Al-Faqurue <sakif.saf915@gmail.com>
2023-01-09 09:04:11 +0600

    talks part is completed

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,24 @@ $('.banner-slick-slider').slick({
     nextArrow: '<span class="next-arr"><i class="fa-solid fa-angle-down"></i></span>',
     vertical: true
   });
+
+  //talks
+  $('.talks-writting-holder').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.talks-picture-holder'
+  });
+  $('.talks-picture-holder').slick({
+    centerPadding: '0px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.talks-writting-holder',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    prevArrow: '<span class="prev-arr"><i class="fa-solid fa-angle-up"></i></i></span>',
+    nextArrow: '<span class="next-arr"><i class="fa-solid fa-angle-down"></i></span>',
+    vertical: true
+  });
